0b9e8d4d5e8372e44534864685594e478143414c
Rohan317685 <dsairohan@gmail.com>
2026-06-26 12:42:43 +0100

    Fix iframe cookie support (SameSite=None for production)

--- src/web/server.ts
+++ src/web/server.ts
@@ -264,8 +264,9 @@ export function createWebServer(app?: App): express.Application {
       resave: false,
       saveUninitialized: false,
       cookie: {
-        secure: false,
+        secure: process.env.NODE_ENV === 'production',
         httpOnly: true,
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
         maxAge: 24 * 60 * 60 * 1000,
       },
     }),
